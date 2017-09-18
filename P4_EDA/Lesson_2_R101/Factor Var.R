3948e21a94730ba5666751c7c31f873741acb4c3
Lotusfire <zhengweiyu@outlook.com>
2017-09-18 16:16:37 +0800

    20170918161637

--- P4_EDA/Lesson_2_R101/Factor Var.R
+++ P4_EDA/Lesson_2_R101/Factor Var.R
@@ -14,3 +14,5 @@ library(ggplot2)
 qplot(data=reddit, x = age.range)
 
 qplot(data=reddit, x = income.range)
+
+reddit$age.range <- ordered(reddit$age.range, levels = c("Under 18", "18-24", "25-34", "35-44", "45-54", "55-64", "65 or Above"))
