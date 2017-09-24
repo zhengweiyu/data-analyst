de72a474822c8a77f79d978d3950397f38cd78eb
lotusfire <zhengweiyu@outlook.com>
2017-09-24 12:48:13 +0800

    20170924124813

--- P4_EDA/Lesson2/Factor Var.R
+++ P4_EDA/Lesson2/Factor Var.R
@@ -1,4 +1,4 @@
-setwd('/Users/lotusfire/workspace/data-analyst/P4_EDA/Lesson_2_R101')
+setwd('C:/Users/lotusfire/workspace/data-analyst/P4_EDA/Lesson_2_R101')
 getwd()
 
 reddit <- read.csv('reddit.csv') 
@@ -14,3 +14,5 @@ library(ggplot2)
 qplot(data=reddit, x = age.range)
 
 qplot(data=reddit, x = income.range)
+
+reddit$age.range <- ordered(reddit$age.range, levels = c("Under 18", "18-24", "25-34", "35-44", "45-54", "55-64", "65 or Above"))
